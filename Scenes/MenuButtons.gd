8011fe65f9267f9d347549a70cef0bbf832cd4f7
Loel Joseph Hofileña <91780906+dirtbrick@users.noreply.github.com>
2023-10-08 05:43:42 +0800

    updatez

--- Scenes/MenuButtons.gd
+++ Scenes/MenuButtons.gd
@@ -10,13 +10,25 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+func _on_Start_pressed():
+	get_node("Start").move(Vector2(-576,0))
+	get_node("8)/VideoContainer/VideoPlayer").play()
+	get_node("8)").move(Vector2(0,0))
 
+func _on_Back2_pressed():
+	get_node("Start").move(Vector2(0,0))
+	get_node("8)/VideoContainer/VideoPlayer").stop()
+	get_node("8)").move(Vector2(576,0))
 
 func _on_Settings_pressed():
 	get_node("Start").move(Vector2(-576,0))
 	get_node("Settings").move(Vector2(0,0))
 
-
 func _on_Back_pressed():
 	get_node("Start").move(Vector2(0,0))
 	get_node("Settings").move(Vector2(576,0))
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
